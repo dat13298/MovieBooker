0dc78fefd226cc93c04d84fe95d44e4ce5a4acb4
dat13298 <157354116+dat13298@users.noreply.github.com>
2025-03-14 01:15:00 +0700

    remove RequiredArgsConstructor

--- src/main/java/com/datnt/moviebooker/repository/RefreshTokenRepository.java
+++ src/main/java/com/datnt/moviebooker/repository/RefreshTokenRepository.java
@@ -2,7 +2,6 @@ package com.datnt.moviebooker.repository;
 
 import com.datnt.moviebooker.entity.RefreshToken;
 import com.datnt.moviebooker.entity.User;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
