7274cdf3f3cc39c0848c09f26bb66dcb7d882909
dat13298 <157354116+dat13298@users.noreply.github.com>
2025-03-14 01:15:48 +0700

    add method find user by username

--- src/main/java/com/datnt/moviebooker/repository/UserRepository.java
+++ src/main/java/com/datnt/moviebooker/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.datnt.moviebooker.repository;
 
 import com.datnt.moviebooker.entity.User;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(@NotNull(message = "Username can not null") @Size(max = 50, min = 5, message = "Username must be between 5 and 50 characters") String username);
 }
