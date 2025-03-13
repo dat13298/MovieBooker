c977683b61160c72dcd9eeec412ae9b17c613964
dat13298 <157354116+dat13298@users.noreply.github.com>
2025-03-14 01:00:17 +0700

    CustomUserDetailsService

---
+++ src/main/java/com/datnt/moviebooker/service/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.datnt.moviebooker.service;
+
+import com.datnt.moviebooker.entity.User;
+import com.datnt.moviebooker.repository.UserRepository;
+import com.datnt.moviebooker.security.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    public final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new CustomUserDetails(user);
+    }
+}
