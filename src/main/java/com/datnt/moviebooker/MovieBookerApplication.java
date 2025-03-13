8586fd892f37166c7ee685229e474926aae1ec9a
dat13298 <157354116+dat13298@users.noreply.github.com>
2025-03-13 12:11:16 +0700

    first commit

---
+++ src/main/java/com/datnt/moviebooker/MovieBookerApplication.java
@@ -0,0 +1,13 @@
+package com.datnt.moviebooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBookerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieBookerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/datnt/moviebooker/MovieBookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datnt.moviebooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBookerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
