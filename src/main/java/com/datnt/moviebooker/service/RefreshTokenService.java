e01e3e4e9596f4e2c35f67dde3fc50ca54bbe6af
dat13298 <157354116+dat13298@users.noreply.github.com>
2025-03-13 23:10:15 +0700

    add RefreshTokenService

---
+++ src/main/java/com/datnt/moviebooker/service/RefreshTokenService.java
@@ -0,0 +1,44 @@
+package com.datnt.moviebooker.service;
+
+import com.datnt.moviebooker.entity.RefreshToken;
+import com.datnt.moviebooker.entity.User;
+import com.datnt.moviebooker.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+
+    @Value("${jwt.refresh-token-expiration}")
+    private long refreshTokenExpiration;
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken createRefreshToken(User user) {
+        RefreshToken refreshToken = RefreshToken.builder()
+                .user(user)
+                .token(UUID.randomUUID().toString())
+                .expiresAt(Timestamp.from(Instant.now().plusMillis(refreshTokenExpiration)))
+                .build();
+
+        return refreshTokenRepository.save(refreshToken);
+    }
+
+    public boolean isTokenExpired(RefreshToken refreshToken) {
+        return refreshToken.getExpiresAt().before(Timestamp.from(Instant.now()));
+    }
+
+    public void deleteTokenByUser(User user) {
+        refreshTokenRepository.deleteByUser(user);
+    }
+
+    public RefreshToken findByToken(String token) {
+        return refreshTokenRepository.findByToken(token).orElse(null);
+    }
+}
