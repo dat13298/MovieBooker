c33ffe815367065489c8559dbc6df5bb8a55aebb
dat13298 <157354116+dat13298@users.noreply.github.com>
2025-03-14 01:17:17 +0700

    insert user for login test

---
+++ src/test/java/com/datnt/moviebooker/UserRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.datnt.moviebooker;
+
+import com.datnt.moviebooker.constant.Role;
+import com.datnt.moviebooker.entity.User;
+import com.datnt.moviebooker.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void testInsertUser() {
+        User user = User.builder()
+                .username("admin")
+                .password("password123")
+                .email("datnt@gmail.com")
+                .role(Role.ROLE_ADMIN)
+                .createdAt(Timestamp.from(Instant.now()))
+                .build();
+
+        User savedUser = userRepository.save(user);
+
+        assertThat(savedUser).isNotNull();
+        assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+}
