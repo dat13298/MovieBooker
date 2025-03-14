5f4c74cecd0df4edf2ad9532a49ae86582cbc6e5
dat13298 <157354116+dat13298@users.noreply.github.com>
2025-03-14 14:08:01 +0700

    add username and role to token

--- src/main/java/com/datnt/moviebooker/security/JwtFilter.java
+++ src/main/java/com/datnt/moviebooker/security/JwtFilter.java
@@ -1,48 +1,65 @@
 package com.datnt.moviebooker.security;
 
-import com.datnt.moviebooker.service.CustomUserDetailsService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
+import com.datnt.moviebooker.service.UserDetailsServiceImpl;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.Nullable;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
 import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
 
-    private final JwtUtil jwtUtil;
-    private final CustomUserDetailsService userDetailsService;
+    private final JwtService jwtService;
+    private final UserDetailsServiceImpl userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
-        String token = getTokenFromHeader(request);
+                                    @Nullable HttpServletResponse response,
+                                    @Nullable FilterChain filterChain) throws ServletException, IOException {
 
-        if (token != null && jwtUtil.validateToken(token)) {
-            String username = jwtUtil.getUsernameFromToken(token);
+        // Lấy token từ Header
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            assert filterChain != null;
+            filterChain.doFilter(request, response);
+            return;
+        }
 
+        // Cắt bỏ "Bearer "
+        String token = authHeader.substring(7);
+
+        // Parse token để lấy username
+        String username = jwtService.getUsernameFromToken(token);
+
+        // Check xem user đã được authenticate hay chưa
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             var userDetails = userDetailsService.loadUserByUsername(username);
-            var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            // Validate token
+            if (jwtService.validateToken(token, userDetails)) {
+                var authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                // Set vào SecurityContext
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
         }
 
+        assert filterChain != null;
         filterChain.doFilter(request, response);
-    }
-
-    private String getTokenFromHeader(HttpServletRequest request) {
-        String header = request.getHeader("Authorization");
-        if (header != null && header.startsWith("Bearer ")) {
-            return header.substring(7);
-        }
-        return null;
     }
 }
