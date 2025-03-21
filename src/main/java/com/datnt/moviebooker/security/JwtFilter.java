28ffb85f688bc787340d7fccaf75924575bf7059
dat13298 <157354116+dat13298@users.noreply.github.com>
2025-03-21 12:55:48 +0700

    clean code

--- src/main/java/com/datnt/moviebooker/security/JwtFilter.java
+++ src/main/java/com/datnt/moviebooker/security/JwtFilter.java
@@ -1,6 +1,5 @@
 package com.datnt.moviebooker.security;
 
-import com.datnt.moviebooker.service.UserDetailsServiceImpl;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.lang.Nullable;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -20,7 +20,7 @@ import java.io.IOException;
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
-    private final UserDetailsServiceImpl userDetailsService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
