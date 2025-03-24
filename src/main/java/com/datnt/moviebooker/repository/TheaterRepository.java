12ed2a947078e651a5fed4ef1379416bbfe042b5
dat13298 <157354116+dat13298@users.noreply.github.com>
2025-03-25 02:17:12 +0700

    fix generic

--- src/main/java/com/datnt/moviebooker/repository/TheaterRepository.java
+++ src/main/java/com/datnt/moviebooker/repository/TheaterRepository.java
@@ -1,9 +1,9 @@
 package com.datnt.moviebooker.repository;
 
-import com.datnt.moviebooker.entity.Movie;
+import com.datnt.moviebooker.entity.Theater;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TheaterRepository extends JpaRepository<Movie, Long> {
+public interface TheaterRepository extends JpaRepository<Theater, Long> {
 }
