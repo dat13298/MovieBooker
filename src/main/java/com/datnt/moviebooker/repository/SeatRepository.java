ca4dc84ae6f5c07fe105f8dcd57be1d6d4e9bad3
dat13298 <157354116+dat13298@users.noreply.github.com>
2025-03-26 01:10:10 +0700

    change to page return

--- src/main/java/com/datnt/moviebooker/repository/SeatRepository.java
+++ src/main/java/com/datnt/moviebooker/repository/SeatRepository.java
@@ -1,12 +1,12 @@
 package com.datnt.moviebooker.repository;
 
 import com.datnt.moviebooker.entity.Seat;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, Long> {
-    List<Seat> findSeatsByScreen_Id(Long screenId);
+    Page<Seat> findSeatsByScreen_Id(Long screenId, Pageable pageable);
 }
