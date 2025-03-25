84d89d5b71abde2afbae0db4f7240b9981c406cc
dat13298 <157354116+dat13298@users.noreply.github.com>
2025-03-26 01:04:24 +0700

    add method find Booking by UserId

--- src/main/java/com/datnt/moviebooker/repository/BookingRepository.java
+++ src/main/java/com/datnt/moviebooker/repository/BookingRepository.java
@@ -1,9 +1,12 @@
 package com.datnt.moviebooker.repository;
 
 import com.datnt.moviebooker.entity.Booking;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+    Page<Booking> findByUser_Id(Long userId, Pageable pageable);
 }
