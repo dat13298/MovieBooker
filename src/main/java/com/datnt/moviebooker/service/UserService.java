9d0b1a19189b1ff5d67e784ea0f4d3fe34eacf9b
dat13298 <157354116+dat13298@users.noreply.github.com>
2025-03-26 01:11:45 +0700

    add method for rest API

--- src/main/java/com/datnt/moviebooker/service/UserService.java
+++ src/main/java/com/datnt/moviebooker/service/UserService.java
@@ -1,14 +1,25 @@
 package com.datnt.moviebooker.service;
 
+import com.datnt.moviebooker.constant.Role;
+import com.datnt.moviebooker.dto.UserRequest;
+import com.datnt.moviebooker.dto.UserResponse;
 import com.datnt.moviebooker.entity.User;
+import com.datnt.moviebooker.mapper.UserMapper;
 import com.datnt.moviebooker.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     public User findById(Long id) {
         return userRepository.findById(id).orElse(null);
@@ -18,4 +29,47 @@ public class UserService {
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new RuntimeException("User not found"));
     }
+
+    public boolean isAdmin(Long userId) {
+        return findById(userId).getRole().equals(Role.ROLE_ADMIN);
+    }
+
+    public Page<UserResponse> getUsers(Role role, Pageable pageable) {
+        Page<User> users = (role != null) ? userRepository.findUsersByRole(role, pageable) : userRepository.findAll(pageable);
+        return users.map(userMapper::toResponse);
+    }
+
+    public UserResponse getUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        return userMapper.toResponse(user);
+    }
+
+    public UserResponse createUser(UserRequest request, boolean isAdmin) {
+        if (isAdmin && request.getRole() != Role.ROLE_ADMIN) {
+            throw new RuntimeException("Admin can only add another admin!");
+        }
+        Role userRole = isAdmin ? request.getRole() : Role.ROLE_USER;
+
+        User newUser = userMapper.toEntity(request);
+        newUser.setRole(userRole);
+
+        User savedUser = userRepository.save(newUser);
+        return userMapper.toResponse(savedUser);
+    }
+
+    public void deleteUser(Long id) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isEmpty()) {
+            throw new RuntimeException("User not found");
+        }
+        userRepository.deleteById(id);
+    }
+
+    public boolean isCurrentUserAdmin() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getAuthorities().stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals(Role.ROLE_ADMIN.name()));
+    }
+
 }
