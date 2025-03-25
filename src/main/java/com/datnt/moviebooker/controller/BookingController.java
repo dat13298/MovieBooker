70363fe718d4347396fdfd1ed975eeba456a6fbf
dat13298 <157354116+dat13298@users.noreply.github.com>
2025-03-26 01:01:53 +0700

    add rest API

--- src/main/java/com/datnt/moviebooker/controller/BookingController.java
+++ src/main/java/com/datnt/moviebooker/controller/BookingController.java
@@ -2,11 +2,15 @@ package com.datnt.moviebooker.controller;
 
 import com.datnt.moviebooker.dto.BookingMessage;
 import com.datnt.moviebooker.dto.BookingRequest;
+import com.datnt.moviebooker.dto.BookingResponse;
 import com.datnt.moviebooker.kafka.KafkaProducer;
 import com.datnt.moviebooker.service.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -15,15 +19,16 @@ import java.util.UUID;
 public class BookingController {
 
     private final AuthService authService;
+    private final BookingService bookingService;
     private final KafkaProducer kafkaProducer;
     private final RedisService redisService;
 
     @PostMapping("/create")
     public ResponseEntity<?> createBooking(@RequestBody BookingRequest request) {
         Long userId = authService.getCurrentUserId();
-        String bookingId = UUID.randomUUID().toString();  // Create bookingId for tracking booking status
+        String bookingId = UUID.randomUUID().toString();  // create bookingId for tracking booking status
 
-        // check seat availability before booking
+        // check if seat is already booked or not locked
         for (Long seatId : request.seatIds()) {
             String lockedBy = redisService.getData("seat_lock:" + request.showTimeId() + ":" + seatId);
             if (lockedBy == null || !lockedBy.equals(userId.toString())) {
@@ -31,23 +36,61 @@ public class BookingController {
             }
         }
 
-        // Create message
+        // create message and send to Kafka
         BookingMessage message = new BookingMessage();
         message.setBookingId(bookingId);
         message.setUserId(userId);
         message.setShowTimeId(request.showTimeId());
         message.setSeatIds(request.seatIds());
 
-        // Send message to Kafka
         kafkaProducer.sendBookingMessage(message);
 
         return ResponseEntity.ok(bookingId);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getBooking(@PathVariable Long id) {
+        Optional<BookingResponse> bookingResponse = bookingService.getBookingById(id);
+        return bookingResponse.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/my")
+    public ResponseEntity<Page<BookingResponse>> getMyBookings(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+
+        Long userId = authService.getCurrentUserId();
+        Page<BookingResponse> bookings = bookingService.getBookingsByUser(userId, page, size);
+
+        return ResponseEntity.ok(bookings);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> cancelBooking(@PathVariable Long id) {
+        Long userId = authService.getCurrentUserId();
+        bookingService.cancelBooking(id, userId);
+        return ResponseEntity.ok("Booking cancelled successfully.");
+    }
+
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping
+    public ResponseEntity<Page<BookingResponse>> getAllBookings(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size) {
+
+        Page<BookingResponse> bookings = bookingService.getAllBookings(page, size);
+        return ResponseEntity.ok(bookings);
+    }
+
+
+
+
 
     @GetMapping("/status/{bookingId}")
     public ResponseEntity<?> getBookingStatus(@PathVariable String bookingId) {
-        // Get booking status from Redis
+        // get booking status from Redis
         String result = redisService.getData(bookingId);
         if (result == null) {
             return ResponseEntity.ok("Booking is being processed...");
