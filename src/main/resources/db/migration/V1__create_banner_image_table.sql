161abebdecc1b6504f643c82bfda418c1b4e85bf
dat13298 <157354116+dat13298@users.noreply.github.com>
2025-06-10 12:38:14 +0700

    Test: test flyway

--- src/main/resources/db/migration/V1__create_banner_image_table.sql
+++ src/main/resources/db/migration/V1__create_banner_image_table.sql
@@ -1,9 +1,9 @@
-CREATE TABLE banner_image
+CREATE TABLE banner_images
 (
     id          BIGINT AUTO_INCREMENT PRIMARY KEY,
-    image_url   VARCHAR(255),
-    title       VARCHAR(255),
-    description TEXT,
-    user_id     BIGINT NOT NULL,
+    image_url   VARCHAR(255) NOT NULL,
+    title       VARCHAR(100) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    user_id     BIGINT       NOT NULL,
     CONSTRAINT fk_banner_user FOREIGN KEY (user_id) REFERENCES users (id)
 );
